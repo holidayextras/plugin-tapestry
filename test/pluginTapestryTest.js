9de3c48da3ca423711a9c047398c048158965b92
Emily Rumbelow <emily.rumbelow@holidayextras.com>
2014-11-10 11:31:19 +0000

    testing

--- test/pluginTapestryTest.js
+++ test/pluginTapestryTest.js
@@ -1,4 +1,6 @@
 /* jslint node: true */
+/* jshint -W098 */ // Ignore 'is defined but never used' errors
+/* jshint -W030 */ /* Stop linter complaining about expression */
 'use strict';
 
 /*
@@ -13,6 +15,15 @@
 var assert = require( 'assert' );
 var Hapi = require( 'hapi' );
 var sinon = require( 'sinon' );
+
+
+var chai = require( 'chai' );
+var chaiAsPromised = require( 'chai-as-promised' );
+chai.use( chaiAsPromised );
+var should = chai.should();
+
+
+
 var Tapestry = require( 'tapestry' );
 var _ = require( 'lodash' );
 
@@ -49,7 +60,7 @@ describe( 'pluginTapestry', function() {
 
 	describe( '#register', function() {
 		it( 'should allow us to access the plugin off the hapi server', function( done ) {
-			assert.notStrictEqual( undefined, server.plugins[pluginName] );
+			server.plugins[pluginName].should.not.be.undefined;
 			done();
 		} );
 	} );
@@ -57,9 +68,109 @@ describe( 'pluginTapestry', function() {
 	describe( '#makeItSo', function() {
 
 		describe( 'check the function is created', function() {
-
 			it( 'should expose makeItSo as a function on the plugin', function( done ) {
-				assert.equal( typeof server.plugins[pluginName].makeItSo, 'function' );
+				server.plugins[pluginName].makeItSo.should.be.a( 'function' );
+				done();
+			} );
+		} );
+
+		describe( 'end to end to check we fail with a thrown error when options arent valid', function() {
+			it( 'should reject the promise because of invalid options', function() {
+				return server.plugins[pluginName].makeItSo().should.eventually.be.rejected.and.eventually.have.property( 'error' ).that.is.an.instanceof( Error );
+			} );
+
+			it( 'should reject the promise because of invalid options.ids', function() {
+				return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/invalidOptionsInputData' ) ).should.eventually.be.rejected.and.eventually.have.property( 'error' ).that.is.an.instanceof( Error );
+			} );
+
+			it( 'should reject the promise because of invalid options.identifier', function() {
+				return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/invalidOptionsIdentifier' )  ).should.eventually.be.rejected.and.eventually.have.property( 'error' ).that.is.an.instanceof( Error );
+			} );
+		} );
+
+		describe( 'end to end to check no code matches is handled correctly', function() {
+
+			it( 'should resolve with an empty object', function() {
+				return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/noCode' ) ).should.eventually.be.rejected.and.eventually.have.property( 'error' ).that.is.an.instanceof( Error );
+			} );
+		} );
+
+		// describe( 'end to end to check the correct arguments reach the call to tapestry.get', function() {
+
+		// 	before( function() {
+		// 		// spy on the arguments that get to the tapestry.get call
+		// 		tapestryMethodStub = sinon.spy( Tapestry.prototype, 'get' );
+		// 	} );
+
+		// 	it( 'should assign the fixture to the requested input `code`', function() {
+		// 		var expected = loadTestResource( './expected/ABC123Arguments' );
+		// 		return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/ABC123Key' ) ).then( function() {
+		// 			//assert.deepEqual( Tapestry.prototype.get.getCall( 0 ).args[0], expected );
+		// 		}, function( error ) {
+		// 			//assert.fail( error, expected );
+		// 		} );
+		// 	} );
+
+		// 	after( function() {
+		// 		tapestryMethodStub.restore();
+		// 	} );
+		// } );
+
+
+
+				// var expected = loadTestResource( './expected/noResult' );
+				// return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/noCode' )  ).should.eventually.be.fulfilled.and.eventually.deep.equal( expected );
+
+		// describe( 'end to end to check the result gets handled correctly from the call to tapestry.get', function() {
+
+		// 	before( function() {
+		// 		// make tapestry.get return the values we can test against
+		// 		tapestryMethodStub = sinon.stub( Tapestry.prototype, 'get' ).callsArgWith( 1, null, loadTestResource( './fixtures/ABC123Content' ) );
+		// 	} );
+
+		// 	it( 'should bind the expected content to the requested fixture input `code`', function() {
+		// 		var expected = loadTestResource( './expected/ABC123Result' );
+		// 		return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/ABC123Key' ) ).then( function( result ) {
+		// 			assert.deepEqual( result, expected );
+		// 		}, function( error ) {
+		// 			assert.fail( error, expected );
+		// 		} );
+		// 	} );
+
+		// 	after( function() {
+		// 		tapestryMethodStub.restore();
+		// 	} );
+		// } );
+
+		// describe( 'end to end to check the result gets handled correctly from the call to tapestry.get when no content is found', function() {
+
+		// 	before( function() {
+		// 		// make tapestry.get return the values we can test against
+		// 		tapestryMethodStub = sinon.stub( Tapestry.prototype, 'get' ).callsArgWith( 1, null, loadTestResource( './fixtures/noResult' ) );
+		// 	} );
+
+		// 	it( 'should not add a `code` property', function() {
+		// 		var expected = loadTestResource( './expected/ABC123NoResult' );
+		// 		return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/ABC123Key' ) ).then( function( result ) {
+		// 			assert.deepEqual( result, expected );
+		// 		}, function( error ) {
+		// 			assert.fail( error, expected );
+		// 		} );
+		// 	} );
+
+		// 	after( function() {
+		// 		tapestryMethodStub.restore();
+		// 	} );
+		// } );
+
+	} );
+
+	describe( '#makeItSoComplex', function() {
+
+		describe( 'check the function is created', function() {
+
+			it( 'should expose makeItSoComplex as a function on the plugin', function( done ) {
+				server.plugins[pluginName].makeItSoComplex.should.be.a( 'function' );
 				done();
 			} );
 
@@ -68,27 +179,15 @@ describe( 'pluginTapestry', function() {
 		describe( 'end to end to check we fail with a thrown error when options arent valid', function() {
 
 			it( 'should reject the promise because of invalid options', function() {
-				return server.plugins[pluginName].makeItSo().then( function( result ) {
-					assert.fail( result, null, successFiredIncorrectlyMessage );
-				}, function( error ) {
-					assert.equal( error.message, 'invalid options' );
-				} );
+				return server.plugins[pluginName].makeItSoComplex().should.eventually.be.rejected.and.eventually.have.property( 'error' ).that.is.an.instanceof( Error );
 			} );
 
 			it( 'should reject the promise because of invalid options.inputData', function() {
-				return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/invalidOptionsInputData' ) ).then( function( result ) {
-					assert.fail( result, null, successFiredIncorrectlyMessage );
-				}, function( error ) {
-					assert.equal( error.message, 'invalid options.inputData' );
-				} );
+				return server.plugins[pluginName].makeItSoComplex( loadTestResource( './fixtures/invalidOptionsInputData' ) ).should.eventually.be.rejected.and.eventually.have.property( 'error' ).that.is.an.instanceof( Error );
 			} );
 
 			it( 'should reject the promise because of invalid options.identifier', function() {
-				return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/invalidOptionsIdentifier' ) ).then( function( result ) {
-					assert.fail( result, null, successFiredIncorrectlyMessage );
-				}, function( error ) {
-					assert.equal( error.message, 'invalid options.identifier' );
-				} );
+				return server.plugins[pluginName].makeItSoComplex(  loadTestResource( './fixtures/invalidOptionsIdentifier' ) ).should.eventually.be.rejected.and.eventually.have.property( 'error' ).that.is.an.instanceof( Error );
 			} );
 
 		} );
@@ -97,11 +196,7 @@ describe( 'pluginTapestry', function() {
 
 			it( 'should resolve with an empty object', function() {
 				var expected = loadTestResource( './expected/noResult' );
-				return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/noCode' ) ).then( function( result ) {
-					assert.deepEqual( result, expected );
-				}, function( error ) {
-					assert.fail( error, expected );
-				} );
+				return server.plugins[pluginName].makeItSoComplex( loadTestResource( './fixtures/noCode' )  ).should.eventually.be.fulfilled.and.eventually.deep.equal( expected );
 			} );
 
 		} );
@@ -115,7 +210,7 @@ describe( 'pluginTapestry', function() {
 
 			it( 'should assign the fixture to the requested input `code`', function() {
 				var expected = loadTestResource( './expected/ABC123Arguments' );
-				return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/ABC123Key' ) ).then( function() {
+				return server.plugins[pluginName].makeItSoComplex( loadTestResource( './fixtures/ABC123Key' ) ).then( function() {
 					assert.deepEqual( Tapestry.prototype.get.getCall( 0 ).args[0], expected );
 				}, function( error ) {
 					assert.fail( error, expected );
@@ -137,7 +232,7 @@ describe( 'pluginTapestry', function() {
 
 			it( 'should bind the expected content to the requested fixture input `code`', function() {
 				var expected = loadTestResource( './expected/ABC123Result' );
-				return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/ABC123Key' ) ).then( function( result ) {
+				return server.plugins[pluginName].makeItSoComplex( loadTestResource( './fixtures/ABC123Key' ) ).then( function( result ) {
 					assert.deepEqual( result, expected );
 				}, function( error ) {
 					assert.fail( error, expected );
@@ -159,7 +254,7 @@ describe( 'pluginTapestry', function() {
 
 			it( 'should not add a `code` property', function() {
 				var expected = loadTestResource( './expected/ABC123NoResult' );
-				return server.plugins[pluginName].makeItSo( loadTestResource( './fixtures/ABC123Key' ) ).then( function( result ) {
+				return server.plugins[pluginName].makeItSoComplex( loadTestResource( './fixtures/ABC123Key' ) ).then( function( result ) {
 					assert.deepEqual( result, expected );
 				}, function( error ) {
 					assert.fail( error, expected );
